5adcfb446335a44fc499215bf0bb7bbb917810be
alaeddin <alaeddin@ahlab.org>
2024-07-10 15:17:04 +0900

    cleanup

--- Assets/Scripts/ChairRotation.cs
+++ Assets/Scripts/ChairRotation.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.XR;
 
 public class ChairRotation : MonoBehaviour
 {
@@ -17,8 +18,7 @@ public class ChairRotation : MonoBehaviour
     public float yController = 0.0f;
     public float yControllerLast = 0.0f;
 
-    private float yRotation = 0.0f;
-    private float yRotationNormalized = 0.0f;
+    public float yRotationNormalized = 0.0f;
 
     static float NormalizeAngle(float angle)
     {
@@ -39,33 +39,39 @@ public class ChairRotation : MonoBehaviour
         bool isRotating = false;
         Vector3 rotationDelta = Vector3.zero;
 
-        yRotation = transform.rotation.eulerAngles.y;
-        yRotationNormalized = NormalizeAngle(transform.rotation.eulerAngles.y);
+        
 
         // Rotate the chair around the y-axis
         if (Input.GetKey(KeyCode.LeftArrow))
         {
             rotationDelta = Vector3.up * -rotationSpeed * Time.deltaTime;
-            // transform.Rotate(Vector3.up, -rotationSpeed * Time.deltaTime);
             isRotating = true;
         }
 
         if (Input.GetKey(KeyCode.RightArrow))
         {
             rotationDelta = Vector3.up * rotationSpeed * Time.deltaTime;
-            // transform.Rotate(Vector3.up, rotationSpeed * Time.deltaTime);
             isRotating = true;
         }
 
+        Quaternion q = transform.rotation * Quaternion.Euler(rotationDelta);
+
+        yRotationNormalized = NormalizeAngle(q.eulerAngles.y);
+
+        
         if (isRotating && IsValidRotation(yRotationNormalized))  
         {
             transform.Rotate(rotationDelta);
+
+            yRotationNormalized = NormalizeAngle(transform.rotation.eulerAngles.y);
+
             this.OnChairRotate?.Invoke(rotationDelta, yRotationNormalized);
         }
 
         if (controllerTransform != null)
         {
-            yController = controllerTransform.rotation.eulerAngles.y;
+            Vector3 eularController = controllerTransform.rotation.eulerAngles;
+            yController = eularController.y;
             yController = NormalizeAngle(yController);
 
             if (yControllerLast != yController 
@@ -77,7 +83,7 @@ public class ChairRotation : MonoBehaviour
                     this.transform.eulerAngles.x,
                     yController,
                     this.transform.eulerAngles.z);
-                    
+
                 this.OnChairRotate?.Invoke(Vector3.zero, yController);
             }
         }
